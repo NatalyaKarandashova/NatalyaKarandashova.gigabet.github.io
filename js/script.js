4bdfa3f87cc2cad44085f9e74fcead2b7fcfa7a1
NatalyaKarandashova <114809586+NatalyaKarandashova@users.noreply.github.com>
2023-01-11 10:53:44 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        adaptiveHeight: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinire: true,
+        centerMode: true,
+        /* responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ] */
+      });
+  });
